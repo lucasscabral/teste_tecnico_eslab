b6e447a0000e5ac236a74709ba4e5ba99c949f55
lucasscabral <lucas.9cabral@gmail.com>
2025-02-12 20:51:09 -0300

    feat: Implementa o app/index do sistema, adicionando layout e pagina no template.

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ClientOnly>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </ClientOnly>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: 'Teste - Gerenciador de Tarefas',
+});
+
+</script>
