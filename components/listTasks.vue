9e1bf15e4f8232af928c4f5ed2bd653a517256cc
lucasscabral <lucas.9cabral@gmail.com>
2025-02-12 20:53:31 -0300

    feat: Implementa o componente de listagem das tarefas e com o componente de pesquisar tarefa.

---
+++ components/listTasks.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col justify-between w-full mt-6 px-2">
+        <h3 class="text-center text-2xl font-bold text-gray-700 dark:text-gray-200">Lista de Tarefas</h3>
+        <!-- Componente de Pesquisar Tarefas e Filtrar-->
+        <SearchAndFilterTask />
+        <!-- Listar Tarefas -->
+        <ul v-auto-animate>
+            <li v-for="task in taskStore.filteredTasks" :key="task.id">
+                <Task :task="task" />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+const taskStore = useTaskStore()
+
+</script>
