5c56743506dd23f7a93594a4a3d6fbcfc779fa32
lucasscabral <lucas.9cabral@gmail.com>
2025-02-14 00:03:00 -0300

    refactor: Ajusta o lado da lupa de pesquisar, por questão de padrão de organização dos botões de ação.

--- components/searchAndFilterTask.vue
+++ components/searchAndFilterTask.vue
@@ -2,11 +2,11 @@
     <div
         class="bg-gray-200 mt-4 mb-4 rounded-md w-full flex flex-col md:flex-row justify-between items-center py-6 px-4 gap-4">
         <div class="flex items-center gap-x-4 w-full">
-            <Icon name="fluent-emoji-flat:magnifying-glass-tilted-right" class="text-3xl text-gray-700 cursor-pointer"
-                @click="handleSearchTask" />
-            <input type="text" placeholder="Pesquisar tarefa" id="Pesquisar"
+            <input type="text" placeholder="Pesquisar tarefa pelo título" id="Pesquisar"
                 class="block w-full md:w-full rounded-md bg-white px-4 py-2 text-base text-gray-700 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 transition-all"
-                v-model="searchQueryInput" />
+                v-model="searchQueryInput" @keyup.enter="handleSearchTask" />
+            <Icon name="fluent-emoji-flat:magnifying-glass-tilted-right" class="text-4xl text-gray-700 cursor-pointer"
+                @click="handleSearchTask" />
         </div>
     </div>
 </template>
